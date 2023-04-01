9e63223d769cf2aca086b907eda3bf344ee0f155
Patryk Słowikowski <p.slowikowski.97@gmail.com>
2023-04-01 23:24:55 +0200

    Create class Player, Human, Crupier

---
+++ player.py
@@ -0,0 +1,23 @@
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.cards_in_hand = []
+
+    def take_card(self, card):
+        return self.cards_in_hand.append(card)
+
+    def values_cards_in_hand(self):
+        values = 0
+        for card in self.cards_in_hand:
+            values += card
+        return values
+    
+
+class Human(Player):
+    pass
+
+
+class Croupier(Player):
+    pass
