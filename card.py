f27c4609c994b7e06369fae04c310685214ec3dd
Patryk Słowikowski <p.slowikowski.97@gmail.com>
2023-04-01 20:52:03 +0200

    Add __str__ to Card class

--- card.py
+++ card.py
@@ -1,5 +1,8 @@
 
 class Card:
-    def __init__(self, color, value):
+    def __init__(self, value, color):
         self.color = color
         self.value = value
+
+    def __str__(self):
+        return f"Card: {self.value}{self.color}"
