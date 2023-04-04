818d83ccacf7d80642887257aafa2d6aa0f8b9c0
Patryk Słowikowski <p.slowikowski.97@gmail.com>
2023-04-04 23:01:31 +0200

    Update class Card in card.py and append tests class's Card

--- card.py
+++ card.py
@@ -1,11 +1,31 @@
+class InvalidColor(Exception):
+    pass
+
+
+class InvalidValue(Exception):
+    pass
+
 
 class Card:
+
+    POSSIBLE_VALUES = list(range(2, 11)) + ["J", "Q", "K", "AS"]
+    POSSIBLE_COLORS = ["♥", "♦", "♠", "♣"]
+
     def __init__(self, value, color):
-        self.color = color
-        self.value = value
+        if color in Card.POSSIBLE_COLORS:
+            self.color = color
+        else:
+            raise InvalidColor("Invalid card color!")
+        if value in Card.POSSIBLE_VALUES:
+            self.value = value
+        else:
+            raise InvalidValue("Invalid card value!")
 
     def __str__(self):
         return f"Card: {self.value}{self.color}"
+
+    def __repr__(self):
+        return f"{self.value} -> {self.color}"
 
     def __eq__(self, other):
         return self.value == other
--- card_test.py
+++ card_test.py
@@ -1,7 +1,35 @@
-from card import Card
+from card import Card, InvalidValue, InvalidColor
+import pytest
 
 
 def test_creation():
     card = Card(10, "♥")
     assert card.color == "♥"
     assert card.value == 10
+
+
+def test_creation_wrong_value():
+    with pytest.raises(InvalidValue) as message:
+        card = Card(20, "♥")
+        assert message == "Invalid card value!"
+
+
+def test_creation_wrong_():
+    with pytest.raises(InvalidColor) as message:
+        card = Card(5, "HEART")
+        assert message == "Invalid card value!"
+
+
+def test_card_representation():
+    assert repr(Card(10, "♥")) == "10 -> ♥"
+    assert repr(Card(2, "♣")) == "2 -> ♣"
+    assert repr(Card("K", "♦")) == "K -> ♦"
+    assert repr(Card("AS", "♠")) == "AS -> ♠"
+
+
+def test_card_string():
+    assert str(Card(10, "♥")) == "Card: 10♥"
+    assert str(Card(2, "♣")) == "Card: 2♣"
+    assert str(Card("K", "♦")) == "Card: K♦"
+    assert str(Card("AS", "♠")) == "Card: AS♠"
+
