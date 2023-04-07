3bd9a5a3e4fb12daba89dcb0f1f1e886c79f662f
Patryk Słowikowski <p.slowikowski.97@gmail.com>
2023-04-07 19:39:02 +0200

    Apend in files deck, deck_test docstrings

--- deck.py
+++ deck.py
@@ -1,13 +1,18 @@
-from card import Card
+"""CLass Deck in BlackJack"""
 from random import shuffle
+from card import Card
 
 
 class Deck:
+    """Creation Deck with 52 cards"""
     def __init__(self):
         self.colors_cards = ["♥", "♦", "♠", "♣"]
         self.values_cards = [*range(2, 11)]
         self.values_cards.extend(["J", "Q", "K", "AS"])
-        self.deck = [Card(value, color) for value in self.values_cards for color in self.colors_cards]
+        self.deck = [Card(value, color)
+                     for value in self.values_cards
+                     for color in self.colors_cards
+                     ]
 
     def __str__(self):
         output = "Talia kart:\n"
@@ -16,7 +21,9 @@ class Deck:
         return output
 
     def shuffle_deck(self):
+        """Function which shuffle all card in deck"""
         shuffle(self.deck)
 
     def pull_out_single_card(self):
+        """Function which pull out single card from deck"""
         return self.deck.pop(0)
--- deck_test.py
+++ deck_test.py
@@ -1,18 +1,22 @@
+"""Test Class Deck"""
 from deck import Deck
 
 
 def test_create_deck():
+    """Test creation Deck"""
     deck = Deck()
     assert len(deck.deck) == 52
 
 
 def test_single_color_deck():
+    """Test correct numbers of single color in deck"""
     deck = Deck()
     heart_cards = [card for card in deck.deck if card.color == "♥"]
     assert len(heart_cards) == 13
 
 
 def test_shuffle():
+    """Test shuffle deck"""
     deck_not_shuffle = Deck()
     deck_with_shuffle = Deck()
     assert deck_not_shuffle.deck == deck_with_shuffle.deck
@@ -21,6 +25,7 @@ def test_shuffle():
 
 
 def test_pull_out_single_card():
+    """Test pull out single card from deck"""
     deck = Deck()
     card_out_deck = deck.pull_out_single_card()
     if card_out_deck in deck.deck:
@@ -28,6 +33,7 @@ def test_pull_out_single_card():
 
 
 def test_no_full_deck():
+    """Check amount card in deck when single card out"""
     deck = Deck()
     deck.pull_out_single_card()
     assert len(deck.deck) == 51
